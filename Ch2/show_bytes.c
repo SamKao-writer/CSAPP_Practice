46f4c168507ef366ed300e674517c7a65383af1f
Sam Kao <sam8409044601@gmail.com>
2024-10-10 19:01:21 +0800

    Add show_bytes.c and ,clang-format in the project.

---
+++ Ch2/show_bytes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void show_bytes(unsigned char* start, size_t len);
+
+int main(int argc, char* argv[]) {
+    int a = 0xFF;
+    show_bytes((unsigned char*)&a, sizeof(a));
+    return 0;
+}
+
+void show_bytes(unsigned char* start, size_t len) {
+    int i;
+    for (i = 0; i < len; i++)
+        printf(" %.2x", *(start + i));
+    printf("\n");
+}
