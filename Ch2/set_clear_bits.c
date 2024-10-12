8450be0718f76d883c9d6be240962d9b5e01aa88
Sam Kao <sam8409044601@gmail.com>
2024-10-12 15:46:01 +0800

    Modify set_clear_bits.c Use preprocessor technique to shorten code.

--- Ch2/set_clear_bits.c
+++ Ch2/set_clear_bits.c
@@ -4,27 +4,26 @@
 #define CLR_BIT(x, pos) (x &= ~(1 << pos))
 #define GET_BIT(x, pos) (!!(x & (1 << pos)))
 
+#define PRINT(type, a)                                       \
+    for (int i = (int) (sizeof(type) << 3) - 1; i >= 0; i--) \
+        printf("%2d", GET_BIT(a, i));                        \
+    printf("\n")
+
 int main(int argc, char *argv[])
 {
     unsigned char a = 0xF0;
     unsigned char pos = 3;
-    
+
     // Try to print out the current bit-level representation
-    for (int i = (int)(sizeof(unsigned char) << 3) - 1; i >= 0; i--)
-        printf("%2d", GET_BIT(a, i));
-    printf("\n");
+    PRINT(unsigned char, a);
 
     // Set a bit and print
     SET_BIT(a, pos);
-    for (int i = (int)(sizeof(unsigned char) << 3) - 1; i >= 0; i--)
-        printf("%2d", GET_BIT(a, i));
-    printf("\n");
+    PRINT(unsigned char, a);
 
     // Clear a bit and print
     CLR_BIT(a, pos + 2);
-    for (int i = (int)(sizeof(unsigned char) << 3) - 1; i >= 0; i--)
-        printf("%2d", GET_BIT(a, i));
-    printf("\n");
+    PRINT(unsigned char, a);
 
     return 0;
 }
