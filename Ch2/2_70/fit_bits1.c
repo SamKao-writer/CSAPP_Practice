74c9c457b05094b5397b14ccba4d81e21e5631f0
Sam Kao <sam8409044601@gmail.com>
2024-10-27 12:39:53 +0800

    Add fit_bits1.c in 2_70 Modify the implementation of neg condition.

---
+++ Ch2/2_70/fit_bits1.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdio.h>
+
+int fit_bits(int x, int n)
+{
+    int pos = !(x >> (n - 1));
+    int neg = !(~x >> (n - 1));
+    return pos || neg;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(!fit_bits(3, 1));
+    assert(fit_bits(3, 3));
+    return 0;
+}
