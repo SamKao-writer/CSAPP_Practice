5b590483207af1db5eed0c6fd5e873936922b5ef
Sam Kao <sam8409044601@gmail.com>
2024-10-27 11:32:26 +0800

    Modify rotate_left1.c in 2_69 Add the testcase of UINT32_MAX.

--- Ch2/2_69/rotate_left1.c
+++ Ch2/2_69/rotate_left1.c
@@ -1,43 +1,48 @@
-#include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
 
-#define DATATYPE_LIST  \
-    X(8)               \
-    X(16)              \
-    X(32)              \
+#define DATATYPE_LIST \
+    X(8)              \
+    X(16)             \
+    X(32)             \
     X(64)
 
 /* Function prototype */
-#define X(num) static inline uint##num##_t rotate_left_##num(uint##num##_t x, int n);
+#define X(num) \
+    static inline uint##num##_t rotate_left_##num(uint##num##_t x, int n);
 DATATYPE_LIST
 #undef X
 
 /* Function definition */
-#define X(num)              \
+#define X(num)                                                            \
     static inline uint##num##_t rotate_left_##num(uint##num##_t x, int n) \
     {                                                                     \
-        int w = (int)(sizeof(uint##num##_t) << 3);                        \
+        int w = (int) (sizeof(uint##num##_t) << 3);                       \
         return (x << n) | (x >> (w - n - 1) >> 1);                        \
     }
-    DATATYPE_LIST
+DATATYPE_LIST
 #undef X
 
 unsigned rotate_left(unsigned x, int n)
 {
-    int w = (int)(sizeof(unsigned) << 3);
+    int w = (int) (sizeof(unsigned) << 3);
     return (x << n) | (x >> (w - n - 1) >> 1);
 }
 
 int main(int argc, char *argv[])
 {
-    // assert(rotate_left_32(0x12345678, 4) == 0x23456781);
-    // assert(rotate_left_32(0x12345678, 20) == 0x67812345);
+    assert(rotate_left_32(0x12345678, 4) == 0x23456781);
+    assert(rotate_left_32(0x12345678, 20) == 0x67812345);
+    
     for (uint32_t i = 0; i < UINT32_MAX; ++i) {
         for (int j = 0; j <= 32; ++j) {
             assert(rotate_left_32(i, j) == rotate_left(i, j));
         }
     }
+
+    for (int j = 0; j <= 32; ++j)
+        assert(rotate_left_32(UINT32_MAX, j) == rotate_left(UINT32_MAX, j));
 
     return 0;
 }
