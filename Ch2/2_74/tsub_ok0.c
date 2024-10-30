024eafabcab3cb809da671fcd9f55d4a71ecdd45
Sam Kao <sam8409044601@gmail.com>
2024-10-30 21:04:50 +0800

    Add tsub_ok0.c in 2_74 The initial thought of the implementation.

---
+++ Ch2/2_74/tsub_ok0.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+
+/* Determine whether arguments can be subtracted without overflow
+ * This function should return 1 if the computation of x-y does not overflow
+ */
+int tsub_ok(int x, int y)
+{
+    int sum = x - y;
+    int mask = INT_MIN;
+    int pos_overflow = !(x & mask) && (y & mask) && (sum & mask);
+    int neg_overflow = (x & mask) && !(y & mask) && !(sum & mask);
+    return !pos_overflow && !neg_overflow;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(!tsub_ok(INT_MAX, -1));
+    assert(!tsub_ok(INT_MIN, 1));
+    assert(tsub_ok(INT_MAX, 1));
+    assert(tsub_ok(INT_MIN, -1));
+    
+    return 0;
+}
