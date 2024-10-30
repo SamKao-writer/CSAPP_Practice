920e0b372ff6d74a0950664c11a371bdb3f7a7e7
Sam Kao <sam8409044601@gmail.com>
2024-10-30 21:36:32 +0800

    Add test cases in tsub_ok0.c in 2_74

--- Ch2/2_74/tsub_ok0.c
+++ Ch2/2_74/tsub_ok0.c
@@ -20,6 +20,10 @@ int main(int argc, char *argv[])
     assert(!tsub_ok(INT_MIN, 1));
     assert(tsub_ok(INT_MAX, 1));
     assert(tsub_ok(INT_MIN, -1));
-    
+    assert(!tsub_ok(0, INT_MIN));
+    assert(tsub_ok(0, INT_MAX));
+    assert(tsub_ok(INT_MIN, 0));
+    assert(tsub_ok(INT_MAX, 0));
+
     return 0;
 }
