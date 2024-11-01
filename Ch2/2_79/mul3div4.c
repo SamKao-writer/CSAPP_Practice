f5cb955f895dcb06e486ad68b1a3b95e3b0684fa
Sam Kao <sam8409044601@gmail.com>
2024-11-01 16:35:34 +0800

    Add mul3div4.c in 2_79 Using bitwise operation to implement integer argument x * 3 / 4.

---
+++ Ch2/2_79/mul3div4.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+
+int divide_power2(int x, int k)
+{
+    int sign_mask = INT_MIN;
+    int is_neg = x & sign_mask;
+    is_neg && (x += ((1 << k) - 1));
+
+    return x >> k;
+}
+
+int mul3div4(int x)
+{
+    /* multiply 3 */
+    x = (x << 1) + x;
+
+    /* divide 4 */
+    return divide_power2(x, 2);
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = INT_MIN; i < INT_MAX; ++i)
+        assert(mul3div4(i) == i * 3 / 4);
+
+    return 0;
+}
